b92f45af4c959e63fbd74cf41e24a1efcddd180f
Julia Sun <julia.sun@berkeley.edu>
2016-07-24 19:25:07 -0700

    Moved hover styling disable to template.jsx

--- pages/index.js
+++ pages/index.js
@@ -12,28 +12,7 @@ export default class Index extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    var touch = window.ontouchstart
-      || (navigator.MaxTouchPoints > 0)
-      || (navigator.msMaxTouchPoints > 0);
-
-    if (touch) { // remove all :hover stylesheets
-      try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-          var styleSheet = document.styleSheets[si];
-          if (!styleSheet.rules) continue;
-
-          for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-            if (!styleSheet.rules[ri].selectorText) continue;
-
-            if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                styleSheet.deleteRule(ri);
-            }
-          }
-        }
-      } catch (ex) {}
-    }
-  }
+  componentDidMount() {}
 
   render () {
     const stringsToType = [

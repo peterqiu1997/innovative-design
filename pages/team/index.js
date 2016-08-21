68a299008d8a33d9d94eb5fa97239cdc1d2a0713
Julia Sun <julia.sun@berkeley.edu>
2016-08-21 13:47:46 -0700

    Updated team quotes and padding

--- pages/team/index.js
+++ pages/team/index.js
@@ -27,18 +27,34 @@ export default class Index extends React.Component {
       },
       {
         name: 'Alven Wang',
+        tagline: 'yolo',
         title: 'Design Services',
         tier: 'Gold Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
         name: 'Natya Dharmosetio',
+        tagline: '✌︎ hehe',
         title: 'Photo Services',
         tier: 'Photo Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
+        name: 'Sonia Hung',
+        title: 'External',
+        tier: 'Gold Tier',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
+        name: 'David Xie',
+        tagline: 'succulent boi',
+        title: 'External',
+        tier: 'Gold Tier',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
         name: 'Roya Chagnon',
+        tagline: 'down with tech!',
         title: 'VP of Marketing',
         tier: 'Marketing Tier',
         photo: '/img/officers/cat.jpg'
@@ -51,34 +67,24 @@ export default class Index extends React.Component {
         photo: '/img/officers/cat.jpg'
       },
       {
-        name: 'Sonia Hung',
-        title: 'External',
-        tier: 'Gold Tier',
-        photo: '/img/officers/cat.jpg'
-      },
-      {
-        name: 'David Xie',
-        title: 'External',
-        tier: 'Gold Tier',
-        photo: '/img/officers/cat.jpg'
-      },
-      {
-        name: 'Brian Giang',
-        title: 'Admin',
-        tier: 'Gold Tier',
+        name: 'Julia Sun',
+        tagline: '1ce qube',
+        title: 'Technology',
+        tier: 'Web Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
         name: 'Arlan Jaska',
+        tagline: '\"><script>alert(/xss/)</script>',
         title: 'Finance',
         tier: 'Web Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
-        name: 'Julia Sun',
-        tagline: '1ce qube',
-        title: 'Technology',
-        tier: 'Web Tier',
+        name: 'Brian Giang',
+        tagline: '*eye emoji*',
+        title: 'Admin',
+        tier: 'Gold Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
@@ -96,24 +102,35 @@ export default class Index extends React.Component {
       },
       {
         name: 'Soph Li',
+        tagline: 'member of the 4am club',
         title: 'Internal',
         tier: 'Photo Tier',
         photo: '/img/officers/cat.jpg'
       },
       {
         name: 'Grace Noh',
+        tagline: 'my wifi password is \'cats are great\'',
         title: 'Education Coordinator',
         tier: 'Intro to Photoshop and Illustrator Decal',
         photo: '/img/officers/cat.jpg'
       },
       {
         name: 'Yudi Sun',
+        tagline: 'shark baby',
         title: 'Education Outreach',
         tier: 'Intro to Photoshop and Illustrator Decal',
         photo: '/img/officers/cat.jpg'
       },
       {
+        name: 'Shota Pangilinan',
+        tagline: 'bad spiderman',
+        title: 'Advisor',
+        tier: 'Photo Tier',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
         name: 'Charisse Celestial',
+        tagline: 'ᕕ( ᐛ )ᕗ',
         title: 'Advisor',
         tier: 'Intro to Photoshop and Illustrator Decal',
         photo: '/img/officers/cat.jpg'
@@ -131,6 +148,13 @@ export default class Index extends React.Component {
         photo: '/img/officers/cat.jpg'
       },
       {
+        name: 'Tina Xu',
+        tagline: 'Generic asian girl (ʘ‿ʘ✿)',
+        title: 'Advisor',
+        tier: 'Blue Tier',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
         name: 'Jeffrey Shi',
         title: 'Facilitator',
         tier: 'Graphic Design Principles Decal',
@@ -138,6 +162,7 @@ export default class Index extends React.Component {
       },
       {
         name: 'Davinci Lam',
+        tagline: 'rave god',
         title: 'Facilitator',
         tier: 'Photo Decal',
         photo: '/img/officers/cat.jpg'
@@ -152,6 +177,24 @@ export default class Index extends React.Component {
         name: 'Allan Peng',
         title: 'Facilitator',
         tier: 'Photo Decal',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
+        name: 'Hongye Xu',
+        tagline: 'pray for me, i\'m about to hit the ye button',
+        title: 'Advisor',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
+        name: 'Ben Marimon',
+        tagline: 'diversity hire',
+        title: 'Advisor',
+        photo: '/img/officers/cat.jpg'
+      },
+      {
+        name: 'Jordan Said',
+        tagline: 'doing too much. asha?',
+        title: 'Advisor',
         photo: '/img/officers/cat.jpg'
       }
     ];

2a04d89ce1e36810ab02947214b5a91f8a594365
Julia Sun <julia.sun@berkeley.edu>
2016-08-20 22:36:00 -0700

    Home page changes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import anime from 'animejs';
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { prefixLink } from 'gatsby-helpers';
@@ -28,24 +29,30 @@ export default class Index extends React.Component {
       <DocumentTitle title="Innovative Design">
         <div>
           <div className="page__wrapper home">
-            <div className="splash__container">
-              <Logo logoType={ LOGO_TEXT } logoClass={ 'logo__svg--color' } />
-              <Typing
-                defaultString="Design"
-                strings={ stringsToType }
-                interval={ { letter: 50, string: 500 } }
-                backspace={ true }
-                cursor={ '|' }
-                startDelay={ 750 }
-                hideCursorOnDone={ true }
-              />
-            </div>
-            <div className="apply__wrapper">
-              <a href="http://apply.innovativedesign.club">
-                <button className="apply__button">
-                  Join Our Team
-                </button>
-              </a>
+            <div className="slideshow">
+              <div className="slide__layout-1">
+                <div className="splash__container">
+                  <Logo logoType={ LOGO_TEXT } logoClass={ 'logo__svg--color' } />
+                  <Typing
+                    defaultString="Design"
+                    strings={ stringsToType }
+                    interval={ { letter: 50, string: 500 } }
+                    backspace={ true }
+                    cursor={ '|' }
+                    startDelay={ 750 }
+                    hideCursorOnDone={ true }
+                  />
+                </div>
+                <div className="slide__button--wrapper">
+                  <div className="slide__button slide__button--1"></div>
+                  <a href="http://apply.innovativedesign.club">
+                    <div className="slide__button slide__button--2">
+                      <div className="apply__text">join our team</div>
+                      <div className="slide__fill"></div>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>

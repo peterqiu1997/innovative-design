dd968eb605b2b3299bfe366f41a1148605a60f81
Julia Sun <julia.sun@berkeley.edu>
2016-08-28 11:36:07 -0700

    Gatsby build and make media links open in new tab

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -61,19 +61,19 @@ export default class Navigation extends React.Component {
                 requests
               </a>
               <div className="nav__item nav__media">
-                <a href="https://www.facebook.com/InnovativeDesignUCB/" className="">
+                <a href="https://www.facebook.com/InnovativeDesignUCB/" target="_blank">
                   <FontAwesome
                     className="media__icon fb"
                     name="facebook"
                   />
                 </a>
-                <a href="https://twitter.com/innodatcal" className="">
+                <a href="https://twitter.com/innodatcal" target="_blank">
                   <FontAwesome
                     className="media__icon twitter"
                     name="twitter"
                   />
                 </a>
-                <a href="https://www.instagram.com/innodatcal/" className="">
+                <a href="https://www.instagram.com/innodatcal/" target="_blank">
                   <FontAwesome
                     className="media__icon ig"
                     name="instagram"

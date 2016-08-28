24d134537bc1915690e49391cd9104ebb747025f
Christian Le <christianle94@gmail.com>
2016-08-27 19:34:49 -0700

    Compression stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const pngcrush = require('imagemin-pngcrush');
 const mozjpeg = require('imagemin-mozjpeg');
+const jpegtran = require('imagemin-jpegtran');
 const svgo = require('imagemin-svgo');
 const gifsicle = require('imagemin-gifsicle');
 
@@ -11,9 +12,10 @@ gulp.task('images', () => {
       progressive: true,
       quality: 50,
       svgoPlugins: [{removeViewBox: false}],
+      optimizationLevel: 7,
       use: [
         pngcrush(),
-        mozjpeg(),
+        jpegtran(),
         svgo(),
         gifsicle()
       ],

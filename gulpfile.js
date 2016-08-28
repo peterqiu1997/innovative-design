2ff90d7a39ea09c798f6c0847755ccb01887a941
Christian Le <christianle94@gmail.com>
2016-08-27 20:33:02 -0700

    Use imageoptim (sorry mac only)

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,9 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const pngcrush = require('imagemin-pngcrush');
-const mozjpeg = require('imagemin-mozjpeg');
-const jpegtran = require('imagemin-jpegtran');
-const svgo = require('imagemin-svgo');
-const gifsicle = require('imagemin-gifsicle');
+const imageoptim = require('gulp-imageoptim');
 
 gulp.task('images', () => {
   return gulp.src('./pages/img/**/*.{jpg,png,svg,gif}', {base: './pages/img'})
-    .pipe(imagemin({
-      progressive: true,
-      quality: 50,
-      svgoPlugins: [{removeViewBox: false}],
-      optimizationLevel: 7,
-      use: [
-        pngcrush(),
-        jpegtran(),
-        svgo(),
-        gifsicle()
-      ],
-      verbose: true
-    }))
+    .pipe(imageoptim.optimize())
     .pipe(gulp.dest('./pages/img'))
 });
 

e8f4a0ae04c3dab524aa6b27a91c2302747010a7
Julia Sun <julia.sun@berkeley.edu>
2016-08-28 17:10:48 -0700

    Gatsby build and disable requests

--- pages/requests/index.js
+++ pages/requests/index.js
@@ -76,7 +76,7 @@ export default class Index extends React.Component {
           })
         }
       >
-        <button id="form__submit" type="submit">
+        <button id="form__submit" type="submit" disabled>
           submit
         </button>
       </div>
@@ -344,7 +344,10 @@ export default class Index extends React.Component {
                   display: this.state.sent && !this.state.sentError ? "none" : "block"
                 }}
               >
-                Please submit design requests through the form below.
+                <b style={{ color: '#F44336' }}>
+                  Requests are closed for Fall 2016 recruitment.  You will not be able to submit requests
+                  through this form until then.  Thank you for your cooperation.
+                </b>
               </div>
               <div className="request__form">
                 { formBody }

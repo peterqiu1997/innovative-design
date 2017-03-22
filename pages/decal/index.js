cb34010e35ee59a950688cd69c46cb5ccbcaa840
Julia Sun <julia.sun@berkeley.edu>
2017-03-22 13:18:38 -0700

    unlock lesson 7

--- pages/decal/index.js
+++ pages/decal/index.js
@@ -55,6 +55,7 @@ export default class Index extends React.Component {
         number: 'Lesson 7',
         title: 'Selection Tools and Masking',
         img: '/img/decal/lesson7.png',
+        url: '/decal/lesson-7/'
       },
       {
         number: 'Lesson 8',

afbe0e69fa67a77ee075bfad1726c4558b8f3bc1
Julia Sun <julia.sun@berkeley.edu>
2017-04-05 14:17:35 -0700

    unlock lesson 8

--- pages/decal/index.js
+++ pages/decal/index.js
@@ -61,6 +61,7 @@ export default class Index extends React.Component {
         number: 'Lesson 8',
         title: 'Blending, Layer Styles and Filter Effects',
         img: '/img/decal/lesson8.png',
+        url: '/decal/lesson-8/'
       },
       {
         number: 'Lesson 9',

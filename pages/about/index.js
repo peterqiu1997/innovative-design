5735c481c5ff07c3a912ae326920aee9513226dd
Peter Qiu <peterqiu1997@gmail.com>
2017-08-27 21:22:33 -0700

    change officer photos

--- pages/about/index.js
+++ pages/about/index.js
@@ -16,183 +16,195 @@ export default class Index extends React.Component {
 
     const officers = [
       {
-        name: 'Jacqueline Woo',
-        tagline: 'Can i get a burley New Orleans w/ light ice to go?',
-        title: 'President',
-        photo: '/img/officers/jackie.jpg'
-      },
-      {
-        name: 'Natasha Welingkar',
-        tagline: 'just your average human bean',
-        title: 'President',
-        photo: '/img/officers/natasha.jpg'
-      },
-      {
-        name: 'Julia Sun',
-        tagline: '1ce qube',
-        title: 'Technology',
-        tier: 'Web Tier',
-        photo: '/img/officers/julia.jpg'
-      },
-      {
-        name: 'Soph Li',
-        tagline: 'speak sophli and carry a big stick',
-        title: 'Photo Services',
-        tier: 'Photo Tier',
-        photo: '/img/officers/soph.jpg'
-      },
-      {
-        name: 'Yudi Sun',
-        tagline: 'shark babying',
-        title: 'External',
-        tier: 'Blue Tier',
-        photo: '/img/officers/yudi.jpg'
-      },
-      {
-        name: 'Lancy Zhang',
-        tagline: 'happily and perpetually lost',
-        title: 'Advisor',
-        tier: 'Blue Tier',
-        photo: '/img/officers/lancy.jpg'
-      },
-      {
-        name: 'David Bui',
-        tagline: 'rain drop, drop top, the sketch file is in my dropbox',
-        title: 'External',
-        tier: 'Web Tier',
-        photo: '/img/officers/davidb.jpg'
-      },
-      {
         name: 'David Xie',
-        tagline: 'YURI!!!onIce',
-        title: 'VP of Marketing',
-        tier: 'Marketing Tier',
-        photo: '/img/officers/davidx.jpg'
-      },
-      {
-        name: 'Tina Xu',
-        tagline: '100% verified tea girl',
-        title: 'VP of Marketing',
-        tier: 'Marketing Tier',
-        photo: '/img/officers/tina.jpg'
-      },
-      {
-        name: 'Arlan Jaska',
-        tagline: 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*',
-        title: 'Finance',
-        tier: 'Web Tier',
-        photo: '/img/officers/arlan.jpg'
-      },
-      {
-        name: 'Barbara Yang',
-        tagline: 'carbs, puppers, pretty colors',
-        title: 'Admin',
-        tier: 'Gold Tier',
-        photo: '/img/officers/barbara.jpg'
-      },
-      {
-        name: 'Charley Huang',
-        tagline: 'Love. That\'s what makes a Subaru a Subaru.',
-        title: 'Recollections',
-        tier: 'Video Tier',
-        photo: '/img/officers/charley.jpg'
+        tagline: '🌓 🌔 🌝 🌖 🌗',
+        title: 'President',
+        photo: '/img/officers/Xie.jpg'
       },
       {
         name: 'Nicholas Chiu',
-        tagline: 'aka chiubacca',
-        title: 'Internal',
-        tier: 'Photo Tier',
-        photo: '/img/officers/nick.jpg'
-      },
-      {
-        name: 'Brian Giang',
-        tagline: 'new father of kitten of unknown gender',
-        title: 'Internal',
-        tier: 'Gold Tier',
-        photo: '/img/officers/brian.jpg'
-      },
-      {
-        name: 'Jessica Kwon',
-        tagline: 'i am a turtle i love u',
-        title: 'Education Coordinator',
-        tier: 'Intro Decal',
-        photo: '/img/officers/jessica.jpg'
-      },
-      {
-        name: 'Brandon Fang',
-        tagline: 'body piercing saved my life',
-        title: 'Education Outreach',
-        tier: 'Gold Tier',
-        photo: '/img/officers/brandon.jpg'
-      },
-      {
-        name: 'Anjile An',
-        tagline: 'will pen tool for food',
-        title: 'Facilitator',
-        tier: 'Intro Decal',
-        photo: '/img/officers/anjile.jpg'
-      },
-      {
-        name: 'Jeffrey Shi',
-        tagline: 'FULLY AUTOMATED GOURMET LUXURY QUEER GREEN SPACE COMMUNISM',
-        title: 'Facilitator and Design Services',
-        tier: 'Graphic Design Principles',
-        photo: '/img/officers/jeffrey.jpg'
-      },
-      {
-        name: 'Khor Le Yi',
-        tagline: 'you can khor me le yi',
-        title: 'Facilitator',
-        tier: 'Graphic Design Principles',
-        photo: '/img/officers/khor.jpg'
+        tagline: 'australian out of water',
+        title: 'President',
+        photo: '/img/officers/Nick.jpg'
       },
       {
         name: 'Jackie Xu',
         tagline: 'who cares about your feed\'s aesthetic',
-        title: 'Facilitator',
-        tier: 'Photo Decal',
-        photo: '/img/officers/jackiex.jpg'
+        title: 'Photo Services',
+        tier: '',
+        photo: '/img/officers/JackieXu.jpg'
       },
       {
-        name: 'Andrew Wang',
-        tagline: 'Follow me on insta @ndrewwang',
-        title: 'Facilitator',
-        tier: 'Photo Decal',
-        photo: '/img/officers/andrew.jpg'
+        name: 'Elizabeth Zhou',
+        tagline: 'they call me ice bear',
+        title: 'Design Services',
+        tier: '',
+        photo: '/img/officers/ElizabethZhou.jpg'
       },
       {
-        name: 'Alven Wang',
-        tagline: 'i am a wang with a huuuuuuuuuge wang ;) muah kisses muah muah muah',
-        title: 'Advisor',
-        tier: 'Gold Tier',
-        photo: '/img/officers/alven.jpg'
+        name: 'Ashley Chen',
+        tagline: 'I love fries so much I can eat them for all three meals a day',
+        title: 'Finance',
+        tier: '',
+        photo: '/img/officers/Ashley.jpg'
+      },
+      {
+        name: 'Julie Wong',
+        tagline: 'Vanilla ice cream is the best ice cream',
+        title: 'Gold Team Leader',
+        tier: '',
+        photo: '/img/officers/Julie.jpg'
+      },
+      {
+        name: 'Essie Xu',
+        tagline: 'A sad Berkeley student ',
+        title: 'Publicity',
+        tier: '',
+        photo: '/img/officers/Essie.jpg'
       },
       {
         name: 'Christine Le',
-        tagline: 'pizza and midday naps',
-        title: 'Facilitator',
-        tier: 'Intro Decal',
-        photo: '/img/officers/christine.jpg'
+        tagline: '(not pictured: a fat slice of pizza)',
+        title: 'Intro Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/ChristineLe.jpg'
+      },
+      {
+        name: 'Jonathan Yang',
+        tagline: 'usher\'s love in this club changed my life',
+        title: 'External',
+        tier: '',
+        photo: '/img/officers/JonYang.jpg'
+      },
+      {
+        name: 'Ellen Hong',
+        tagline: 'hamilton & b l a c k m i r r o r',
+        title: 'Admin',
+        tier: '',
+        photo: '/img/officers/Ellen.jpg'
+      },
+      {
+        name: 'David Nguyen',
+        tagline: 'accepting offerings of chinese express and beverages',
+        title: 'Internal',
+        tier: '',
+        photo: '/img/officers/Eggboy.jpg'
+      },
+      {
+        name: 'Andrew Chang',
+        tagline: 'I live for memes puns and food',
+        title: 'Photo Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/AndrewChang.jpg'
+      },
+      {
+        name: 'Charley Huang',
+        tagline: 'not your average white girl',
+        title: 'Education',
+        tier: '',
+        photo: '/img/officers/Charley.jpg'
+      },
+      {
+        name: 'Soph Li',
+        tagline: 'fruit-eating, flower-smelling, forest-loving, feet-swinging lil nugget',
+        title: 'Internal',
+        tier: '',
+        photo: '/img/officers/Soph.jpg'
+      },
+      {
+        name: 'Brandon Fang',
+        tagline: 'living abundantly',
+        title: 'Education',
+        tier: '',
+        photo: '/img/officers/Brandon.jpg'
+      },
+      {
+        name: 'Peter Qiu',
+        tagline: '🎶',
+        title: 'Tech',
+        photo: '/img/officers/Peter.jpg'
+      },
+      {
+        name: 'Andrew Wang',
+        tagline: 'Don\'t don\'t follow me on instagram @ndrewwang',
+        title: 'Photo Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/AndrewWang.jpg'
+      },
+      {
+        name: 'Jessica Kwon',
+        tagline: 'i like turtles',
+        title: 'Gold Team Leader',
+        tier: '',
+        photo: '/img/officers/JKwon.jpg'
+      },
+      {
+        name: 'Kiana Aryan',
+        tagline: 'lil kiki from 217',
+        title: 'Intro Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/Kiana.jpg'
+      },
+      {
+        name: 'Jackie Zou',
+        tagline: 'witch@berkeley.edu',
+        title: 'Marketing',
+        tier: '',
+        photo: '/img/officers/JackieZou.jpg'
       },
       {
         name: 'Margaret Zhou',
-        tagline: 'kiwikiwikiwi',
-        title: 'Facilitator',
-        tier: 'Intro Decal',
-        photo: '/img/officers/margie.jpg'
+        tagline: 'Soft crunches of the forest carpet and blurred readings of a beautiful design.',
+        title: 'Intro Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/Margie.jpg'
       },
       {
-        name: 'Ankur Maniar',
-        tagline: 'fashion baby FASHUN*~*~*~*~',
-        title: 'Facilitator',
-        tier: 'Photo Decal',
-        photo: '/img/officers/ankur.jpg'
+        name: 'Barbara Yang',
+        tagline: 'flamin hot cheetos in a bonne maman jar',
+        title: 'External',
+        tier: '',
+        photo: '/img/officers/Barbs.jpg'
       },
       {
-        name: 'Ben Marimon',
-        tagline: 'my favorite type of flowers is Brandon Flowers',
+        name: 'Sascha Koller',
+        tagline: '🔩',
+        title: 'Recollections',
+        tier: '',
+        photo: '/img/officers/Sascha.jpg'
+      },
+      {
+        name: 'Jeffrey Shi',
+        tagline: 'Ito En teas, esoteric movies, and good ol\' fashioned home cooking.',
         title: 'Advisor',
-        photo: '/img/officers/ben.jpg'
+        tier: '',
+        photo: '/img/officers/JeffShi.jpg'
+      },
+      {
+        name: 'Roya Chagnon',
+        tagline: 'Urbanism, hawaiian shirts, & chocolate chip cookies',
+        title: 'Intro Decal Facilitator',
+        tier: '',
+        photo: '/img/officers/Roya.jpg'
+      },
+      {
+        name: 'Yudi Sun',
+        tagline: 'yudi moon',
+        title: 'Advisor',
+        tier: '',
+        photo: '/img/officers/Yudi.jpg'
+      },
+      {
+        name: 'Luke Larson',
+        tagline: 'Hard to spot in a snowstorm',
+        title: 'Intro Decal Facilitator',
+        photo: '/img/officers/Luke.jpg'
+      },
+      {
+        name: 'David Bui',
+        tagline: 'Everything In Its Right Place',
+        title: 'Marketing',
+        photo: '/img/officers/bui.jpg'
       }
     ];
 
